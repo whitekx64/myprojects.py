474e3d37a868ce43308b7931fbdc7951e998e0f3
whitekx64 <plackplack558@gmail.com>
2025-09-12 14:25:55 +0300

    Привет 2

--- text.py
+++ text.py
@@ -1,14 +1,19 @@
-def write():
-    
+def question():
+    quest = input("Записать -- 0, прочитать -- 1: ")
+    return quest
+
+def write(text):
+    with open("file.txt", "w") as file:
+        file.write(text) 
+
+def read():
+    with open("file.txt", "r") as file:
+        contant = file.read()
+        print(contant)
 
 def main():
+    question(writetext)
+    write(writetext)
+    read()
 
-    quest = input("Записать -- 0, прочитать -- 1: ")
-
-    with open("file.txt", "w") as file:
-        file.write("я очень люблю милых мальчиков :3 \n") 
-        file.write(":P")
-
-    with open("file.txt", "r") as file:
-        content = file.read()
-        print(content)
+writetext = question
